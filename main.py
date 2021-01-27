76869f82d4d850660da9a76253dad2b032fe9302
Antonin <sohyantonin@gmail.com>
2021-01-27 10:48:49 +0100

    correction

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 import tkinter
-import pygame 
+import pygame
 
-### window initialization and settings ###
-app = tkinter.Tk()
-app.title("MusicApp")
+def main():
+    ''' Main function
+    '''
+    ### window initialization and settings ###
+    app = tkinter.Tk()
+    app.title("MusicApp")
 
-# window logo (faut choisir le bon ..)
-app.iconbitmap('./images/logo2.ico')
+    # window logo (faut choisir le bon ..)
+    app.iconbitmap('./images/logo2.ico')
 
-## center the window ##
+    ## center the window ##
 
-# get  the resolution
-screen_x = int(app.winfo_screenwidth())
-screen_y = int(app.winfo_screenheight())
-window_x = 800
-window_y = 600
+    # get  the resolution
+    screen_x = int(app.winfo_screenwidth())
+    screen_y = int(app.winfo_screenheight())
+    window_x = 800
+    window_y = 600
 
-# calculation to center the window
-position_x = (screen_x // 2) - (window_x //2)
-position_y = (screen_y // 2) - (window_y //2)
+    # calculation to center the window
+    position_x = (screen_x // 2) - (window_x //2)
+    position_y = (screen_y // 2) - (window_y //2)
 
-# create the dimension of the window
-geo = f"{window_x}x{window_y}+{position_x}+{position_y}"
-app.geometry(geo)
-
-
-
-'''
-les appels de fonction ...
-'''
+    # create the dimension of the window
+    geo = f"{window_x}x{window_y}+{position_x}+{position_y}"
+    app.geometry(geo)
 
 
 
+    '''
+    les appels de fonction ...
+    '''
 
-app.mainloop()
+
+
+
+    app.mainloop()
+
+
+
+if __name__ == '__main__':
+    main()
