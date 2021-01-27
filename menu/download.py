5480e276fcc543a76bdbcf75bd92335cb0b2b8e4
Antonin <sohyantonin@gmail.com>
2021-01-27 17:09:28 +0100

    fonction et interface download musique

---
+++ menu/download.py
@@ -0,0 +1,76 @@
+import pytube
+import os
+from moviepy.editor import *
+
+def download_music(video_url, extension):
+    '''Download the video of youtube
+
+    Parameters
+    ----------
+    video_url : the youtube video url (str)
+    extension : the extension of video, mp4 or mp3 (str)
+
+    Note
+    ----
+    Version Windows
+
+    '''
+    # Load url in function youtube
+    youtube = pytube.YouTube(video_url)
+
+    # get the user of the PC
+    user_computer = str(os.environ.get("USERNAME"))
+    path_download = 'C:/Users/' + user_computer + '/Music/Musique_Download'
+
+    # Set streams resolution
+    try:
+        video = youtube.streams.get_by_itag(18)
+        # download video
+        path_mp4 = video.download(path_download)
+
+    except:
+        video = youtube.streams.get_by_itag(140)
+        # download video
+        path_mp4 = video.download(path_download)
+        
+        # impossible to convert because it is already audio
+        exit()
+
+    if extension == 'mp3':
+        # get the base of path
+        base = os.path.basename(path_mp4)
+
+        # get the path of mp3
+        path_mp3 = path_download + '/' + os.path.splitext(base)[0] + '.mp3'
+
+        convert(path_mp4, path_mp3)
+
+        print('Done')
+
+def convert(path_mp4, path_mp3):
+    '''Convert mp4 to mp3
+
+    Parameters
+    ----------
+    path_mp4 : path of mp4 (str)
+    path_mp3 : path of mp3 (str)
+    '''
+
+    # check if the path of mp4 exists
+    if os.path.exists(path_mp4):
+
+        mp3_file = str(path_mp3)
+
+        videoclip = VideoFileClip(path_mp4)
+
+        audioclip = videoclip.audio
+
+        # convert mp4 to mp3
+        audioclip.write_audiofile(mp3_file)
+
+        audioclip.close()
+        videoclip.close()
+
+        # remove the mp4
+        os.remove(path_mp4)
+        
---
+++ menu/window_download.py
@@ -0,0 +1,88 @@
+import tkinter
+import download
+
+def create_window():
+    
+    # create window
+    window_download_music = tkinter.Tk()
+
+    # title of the window
+    window_download_music.title('télécharger musique')
+
+    # window logo
+    # window_download_music.iconbitmap('../images/logo2.ico')
+
+    # dimension of the window
+    window_download_music.geometry('480x200')
+
+    # max and min resolution
+    window_download_music.maxsize(480, 200)
+    window_download_music.minsize(480, 200)
+
+    interface_download(window_download_music)
+
+    window_download_music.mainloop()
+
+
+
+def interface_download(window_download_music):
+    '''download window interface
+
+    Parameters
+    ----------
+    window_download_music : the download window (tkinter)
+    '''
+    # Label
+    label_title = tkinter.Label(window_download_music, text='Download Youtube music', fg='#2980b9', font=('Calibri', 20))
+    label_title.grid(padx=100,pady=8, row=0)
+
+    label_option = tkinter.Label(window_download_music, text='Entrer le lien de la musique :', font=('Calibri', 12))
+    label_option.grid(pady=4, row=1)
+
+    notification = tkinter.Label(window_download_music, font=('Calibri', 12))
+    notification.grid(pady=10, row=4)
+
+    # Entry url
+    url = tkinter.StringVar()
+    entry_url = tkinter.Entry(window_download_music, width=50, textvariable=url)
+    entry_url.grid(row=2)
+    
+
+    # Download button
+
+    download_button = tkinter.Button(window_download_music, width=20, text='Télécharger', font=('Calibri', 12), command=lambda: get_url(url, entry_url, notification))
+    download_button.grid(pady=5, row=3)
+
+    
+def get_url(url, entry_url, notification):
+    '''retrieve the url of the text entry
+
+    Parameter
+    ---------
+    url : the url of the music (str)
+    entry_url : the text area (tkinter)
+    notification : download information (tkinter)
+    '''
+    # get the url of the music
+    music_url = str(url.get())
+
+    try: 
+        # download music
+        download.download_music(music_url, 'mp3')
+
+        # modify the information during execution
+        notification.config(fg='#2ecc71', text='Téléchargement réussi !')
+
+    except:
+        # modify the information during execution
+        notification.config(fg='#e74c3c', text='Téléchargement impossible !')
+
+    # clean the text area
+    entry_url.delete(0, 'end')
+
+
+
+
+create_window()
+
+
