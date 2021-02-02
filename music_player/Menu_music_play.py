010945b5c923c187696064917c512764be06d9db
Antonin <sohyantonin@gmail.com>
2021-02-02 17:43:14 +0100

    add the list of song

--- music_player/Menu_music_play.py
+++ music_player/Menu_music_play.py
@@ -1,5 +1,6 @@
 import tkinter
 import tkinter.ttk
+import music_player.display_song
 
 def music_play_button(app):
     """Display the menu with buttons and the background image
@@ -15,6 +16,7 @@ def music_play_button(app):
     global next_image
     global shuffle_image
     global image_de_fond
+    global list_image
 
     # Création des images
     play_image = tkinter.PhotoImage(file="./music_player/icones_boutons/play.png")
@@ -22,6 +24,7 @@ def music_play_button(app):
     previous_image = tkinter.PhotoImage(file="./music_player/icones_boutons/back.png")
     next_image = tkinter.PhotoImage(file="./music_player/icones_boutons/next.png")
     shuffle_image = tkinter.PhotoImage(file="./music_player/icones_boutons/random.png")
+    list_image = tkinter.PhotoImage(file="./music_player/icones_boutons/list.png")
     #image_de_fond = tkinter.PhotoImage(file="./music_player/icones_boutons/logo Avengers.png")
 
     # # Affichage de l'image de fond
@@ -39,19 +42,18 @@ def music_play_button(app):
     previous_button = tkinter.Button(mainframe, image=previous_image, bg="#010123", borderwidth=0, activebackground="#010123")
     next_button = tkinter.Button(mainframe, image=next_image, bg="#010123", borderwidth=0, activebackground="#010123")
     shuffle_button = tkinter.Button(mainframe, image=shuffle_image, bg="#010123", borderwidth=0, activebackground="#010123")
+    list_button = tkinter.Button(mainframe, image=list_image, bg="#010123", borderwidth=0, activebackground="#010123", command=lambda:music_player.display_song.display_song_box(app, song_box))
 
     volume_button = tkinter.ttk.Scale(mainframe, from_=0, to=100, value=100)
 
     #### DISPLAY ####
     mainframe.pack(side="bottom", pady=50)
 
-    # song box
-    song_box.place(x=0, y=80)
-
     # background image
     #label.pack(pady=20)
 
     # Button
+    list_button.pack(side="left", padx=10, pady=5)
     shuffle_button.pack(side="left", padx=10, pady=5)
     previous_button.pack(side="left", padx=10, pady=5)
     play_button.pack(side="left", padx=10, pady=5)
